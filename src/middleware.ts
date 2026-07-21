5ec3daa0274e5460a3ee5a9cd395b23fd7635d6f
noackjona-hash <noackjona@gmail.com>
2026-07-21 15:14:02 +0200

    Fix middleware intercepting static files like robots.txt

--- src/middleware.ts
+++ src/middleware.ts
@@ -141,6 +141,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|public).*)',
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|txt|xml)$).*)',
   ],
 };
